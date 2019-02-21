9d2f5cfaf0133b01eb9145c6aa0aa55377a0b32b
Cristiam Castillo <ccastillop@gmail.com>
2019-02-21 11:31:36 -0500

    recruty por recluty

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,15 +3,4 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :choice, optional: true
 
-  # validate :check_radio_one_choice_answered
-  # validate :booly_answered
-
-  # def check_radio_one_choice_answered
-  #   errors.add(:choice_id) if (question.kind=="checkbox" || question.kind=="radiobutton") && quiz.answers.select{|a| a.booly==true }.size == 0
-  # end
-
-  # def booly_answered
-  #   errors.add(:booly) if question.kind=="boolean" && booly==nil
-  # end
-
 end
