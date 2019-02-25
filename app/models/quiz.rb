366ebd259039777319984634bece70c55cea716c
Cristiam Castillo <ccastillop@gmail.com>
2019-02-25 01:22:12 -0500

    validación de pregunta opciones testeada

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,7 +4,7 @@ class Quiz < ApplicationRecord
   has_many :answers
   before_save :persist_state
 
-  accepts_nested_attributes_for :answers, reject_if: proc { |atts| atts['choice_id']=="0" }
+  accepts_nested_attributes_for :answers#, reject_if: proc { |atts| atts['choice_id']==nil && atts['booly']==nil }
 
   validate :radios
   validate :booleans
@@ -23,7 +23,7 @@ class Quiz < ApplicationRecord
 
   def mark_error?(question)
     errors.count > 0 && (
-      ("radiobutton"==question.kind && answers.to_a.select{ |a| a.question_id == question.id }.count == 0) || 
+      ("radiobutton"==question.kind && answers.to_a.select{ |a| a.question_id == question.id && a.choice_id==nil}.count > 0) || 
       ("boolean"==question.kind && answers.to_a.select{ |a| a.question_id == question.id && a.booly==nil }.count > 0)  )
   end
 
@@ -37,18 +37,20 @@ class Quiz < ApplicationRecord
   end
 
   def radios
-    q_ids = questionnaire.questions.where(kind: "radiobutton").pluck(:id)
-    #exists one answer per each question
-    if q_ids.select{|q_id| answers.to_a.select{|a| a.question_id==q_id}.count == 0 }.size > 0
-      errors.add(:base, "Elige una opción")
+    questionnaire.questions.where(kind: "radiobutton").each do |question|
+      #must exists at least one answer per each question
+      if answers.to_a.select{|a| a.question_id==question.id && a.choice_id==nil }.size > 0 
+        errors.add(:base, "Elige una opción para '#{question}'")
+      end
     end
   end
 
   def booleans
-    q_ids = questionnaire.questions.where(kind: "boolean").pluck(:id)
-    #exists one answer per each question
-    if q_ids.select{|q_id| answers.to_a.select{ |a| a.question_id==q_id && a.booly==nil }.count > 0 }.size > 0
-      errors.add(:base, "Responde con verdadero o falso")
+    questionnaire.questions.where(kind: "boolean").each do |question|
+      #all answers must be answered with true or false
+      if answers.to_a.select{ |a| a.question_id==question.id && a.booly==nil }.size > 0
+        errors.add(:base, "Responde con verdadero o falso la pregunta: '#{question}' ")
+      end
     end
   end
 
